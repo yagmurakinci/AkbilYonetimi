e3710a6a2715591817d0a6253da73da141e51246
yagmurakinci <yagmurakinci@icloud.com>
2022-12-13 11:51:11 +0300

    frmGiris tasarımı yapıldı

--- AkbilYonetimiFormUI/Form1.Designer.cs
+++ AkbilYonetimiFormUI/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace AkbilYonetimiFormUI
 {
-    partial class Form1
+    partial class FrmGiris
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,98 @@ namespace AkbilYonetimiFormUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGiris));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.btnKayitOl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.label1.Location = new System.Drawing.Point(294, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(294, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(356, 52);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(149, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(356, 82);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(149, 20);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(313, 131);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(143, 23);
+            this.btnGirisYap.TabIndex = 4;
+            this.btnGirisYap.Text = "GİRİŞ YAP";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnKayitOl
+            // 
+            this.btnKayitOl.Location = new System.Drawing.Point(313, 171);
+            this.btnKayitOl.Name = "btnKayitOl";
+            this.btnKayitOl.Size = new System.Drawing.Size(143, 23);
+            this.btnKayitOl.TabIndex = 5;
+            this.btnKayitOl.Text = "KAYIT OL";
+            this.btnKayitOl.UseVisualStyleBackColor = true;
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(581, 296);
+            this.Controls.Add(this.btnKayitOl);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmGiris";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.Button btnKayitOl;
     }
 }
 
--- AkbilYonetimiFormUI/Form1.cs
+++ AkbilYonetimiFormUI/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace AkbilYonetimiFormUI
 {
-    public partial class Form1 : Form
+    public partial class FrmGiris : Form
     {
-        public Form1()
+        public FrmGiris()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
