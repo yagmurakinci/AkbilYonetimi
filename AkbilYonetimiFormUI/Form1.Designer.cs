a7437ab414d34d6a8e109ad33df953cd74ff5cb2
yagmurakinci <yagmurakinci@icloud.com>
2022-12-13 13:19:25 +0300

    frmKayitOl formunun tasarımı yapıldı

--- AkbilYonetimiFormUI/Form1.Designer.cs
+++ AkbilYonetimiFormUI/Form1.Designer.cs
@@ -43,9 +43,10 @@ namespace AkbilYonetimiFormUI
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Cursor = System.Windows.Forms.Cursors.VSplit;
-            this.label1.Location = new System.Drawing.Point(294, 59);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(227, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Email";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -54,29 +55,30 @@ namespace AkbilYonetimiFormUI
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(294, 89);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(227, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Şifre";
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(356, 52);
+            this.txtEmail.Location = new System.Drawing.Point(289, 52);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(149, 20);
             this.txtEmail.TabIndex = 2;
             // 
             // txtSifre
             // 
-            this.txtSifre.Location = new System.Drawing.Point(356, 82);
+            this.txtSifre.Location = new System.Drawing.Point(289, 82);
             this.txtSifre.Name = "txtSifre";
             this.txtSifre.Size = new System.Drawing.Size(149, 20);
             this.txtSifre.TabIndex = 3;
             // 
             // btnGirisYap
             // 
-            this.btnGirisYap.Location = new System.Drawing.Point(313, 131);
+            this.btnGirisYap.Location = new System.Drawing.Point(264, 127);
             this.btnGirisYap.Name = "btnGirisYap";
             this.btnGirisYap.Size = new System.Drawing.Size(143, 23);
             this.btnGirisYap.TabIndex = 4;
@@ -86,7 +88,7 @@ namespace AkbilYonetimiFormUI
             // 
             // btnKayitOl
             // 
-            this.btnKayitOl.Location = new System.Drawing.Point(313, 171);
+            this.btnKayitOl.Location = new System.Drawing.Point(264, 167);
             this.btnKayitOl.Name = "btnKayitOl";
             this.btnKayitOl.Size = new System.Drawing.Size(143, 23);
             this.btnKayitOl.TabIndex = 5;
@@ -99,7 +101,7 @@ namespace AkbilYonetimiFormUI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(581, 296);
+            this.ClientSize = new System.Drawing.Size(460, 288);
             this.Controls.Add(this.btnKayitOl);
             this.Controls.Add(this.btnGirisYap);
             this.Controls.Add(this.txtSifre);
